700ed2a7a02ff7d76f2ab87e48a2c8ca976e4a33
Ekaterina-byte <max-ka82@mail.ru>
2024-07-20 20:26:38 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,12 @@
+immutable_var=([1,2,3], 4, True)
+print(immutable_var)
+immutable_var[0][0]=5
+print(immutable_var)
+mutable_list=[1,2,3,4,5]
+mutable_list[0]=5
+mutable_list[1]=4
+mutable_list[2]=3
+mutable_list[3]=2
+mutable_list[4]=1
+print(mutable_list)
+
